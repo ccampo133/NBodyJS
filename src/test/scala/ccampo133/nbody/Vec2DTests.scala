a212b3a54f398034ff94c39b3a355f746edf8aa8
Chris Campo <ccampo133@users.noreply.github.com>
2015-02-08 16:45:31 -0500

    Formatting.

--- src/test/scala/ccampo133/nbody/Vec2DTests.scala
+++ src/test/scala/ccampo133/nbody/Vec2DTests.scala
@@ -16,6 +16,7 @@ object Vec2DTests extends TestSuite {
       assert(math.abs(v3.x - 3) < epsilon)
       assert(math.abs(v3.y - 4.4) < epsilon)
     }
+
     'testSubtract {
       val v1 = new Vec2D(1.5, 2.0)
       val v2 = new Vec2D(1.0, 1.2)
@@ -23,28 +24,33 @@ object Vec2DTests extends TestSuite {
       assert(math.abs(v3.x - 0.5) < epsilon)
       assert(math.abs(v3.y - 0.8) < epsilon)
     }
+
     'testScalarMultiply {
       val v1 = new Vec2D(1.1, 2.2)
       val v2 = v1 * 10
       assert(math.abs(v2.x - 11) < epsilon)
       assert(math.abs(v2.y - 22) < epsilon)
     }
+
     'testDivide {
       val v1 = new Vec2D(11, 22)
       val v2 = v1 / 10
       assert(math.abs(v2.x - 1.1) < epsilon)
       assert(math.abs(v2.y - 2.2) < epsilon)
     }
+
     'testDot {
       val v1 = new Vec2D(10, 15)
       val v2 = new Vec2D(1, 2)
       val dotProduct = v1 dot v2
       assert(math.abs(dotProduct - 40) < epsilon)
     }
+
     'testLength {
       val v1 = new Vec2D(1, 2)
       assert(math.abs(v1.length() - math.sqrt(5)) < epsilon)
     }
+
     'testGetUnitVector {
       val v1 = new Vec2D(3, 5)
       val v2 = Vec2D.getUnitVector(v1)
