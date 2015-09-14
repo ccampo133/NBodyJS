5810826f100735b8aec9da9e5104d7c7179c8b16
Chris Campo <ccampo133@users.noreply.github.com>
2015-09-14 10:52:12 -0400

    Minor style change

--- src/main/scala/ccampo133/nbody/Physics.scala
+++ src/main/scala/ccampo133/nbody/Physics.scala
@@ -17,6 +17,6 @@ object Physics {
       val r12 = body2.position - pos
       r12 * body2.mass * G / math.pow(r12.length, 2)
     }
-    (bodies map gravity(pos)).sum
+    bodies map gravity(pos) reduceLeft(_ + _)
   }
 }
